e494c12bce677c23511c3cd2ebdee9956825a008
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-08-15 09:41:36 +0200

    Java

---
+++ quantity-store/src/main/java/com/damjan/quantitystore/QuantityStoreApplication.java
@@ -0,0 +1,13 @@
+package com.damjan.quantitystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantityStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantityStoreApplication.class, args);
+	}
+
+}
