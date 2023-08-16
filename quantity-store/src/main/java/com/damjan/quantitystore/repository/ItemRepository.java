785859866030a46ea8bce689ec66dbae37c5cd61
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-08-16 14:05:51 +0200

    Modified class

--- quantity-store/src/main/java/com/damjan/quantitystore/repository/ItemRepository.java
+++ quantity-store/src/main/java/com/damjan/quantitystore/repository/ItemRepository.java
@@ -2,7 +2,6 @@ package com.damjan.quantitystore.repository;
 
 import com.damjan.quantitystore.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
