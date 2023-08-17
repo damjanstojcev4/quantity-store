6fb5c442756c2f50811fda01a02945cab5dcd651
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-08-17 15:55:34 +0200

    Controller layer

---
+++ quantity-store/src/main/java/com/damjan/quantitystore/controller/ItemController.java
@@ -0,0 +1,22 @@
+package com.damjan.quantitystore.controller;
+
+import com.damjan.quantitystore.domain.Item;
+import com.damjan.quantitystore.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/linkesti")
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/")
+    public List<Item> getAllItems() {
+        return itemService.getAllItems();
+    }
+}
