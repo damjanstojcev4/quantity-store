aa8a2089ea71bef09d4d463766ea226127c18f3b
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-08-18 11:26:36 +0200

    FindBySku

--- quantity-store/src/main/java/com/damjan/quantitystore/repository/ItemRepository.java
+++ quantity-store/src/main/java/com/damjan/quantitystore/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package com.damjan.quantitystore.repository;
 
 import com.damjan.quantitystore.domain.Item;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,9 +13,9 @@ import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
     List<Item>findAll();
-    List<Item>findBySku(Long sku);
-//    Optional<Item>findById(Integer id);
-//    @Modifying
-//    @Query("UPDATE Item i SET i.quantity = :newQuantity WHERE i.itemId = :itemId")
-//    void updateQuantity(Integer itemId);
+    Item findBySku(Long sku);
+    @Modifying
+    @Query("UPDATE Item i SET i.quantity = :newQuantity WHERE i.itemId = :itemId")
+    void updateQuantity(Integer itemId);
+
 }
