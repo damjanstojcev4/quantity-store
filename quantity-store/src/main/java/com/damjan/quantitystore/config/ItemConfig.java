ad0c90b20de269d0c9f20d65ba5a04ef17616df3
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-08-18 11:01:06 +0200

    Added New Items

--- quantity-store/src/main/java/com/damjan/quantitystore/config/ItemConfig.java
+++ quantity-store/src/main/java/com/damjan/quantitystore/config/ItemConfig.java
@@ -16,30 +16,79 @@ public class ItemConfig {
     CommandLineRunner commandLineRunner(ItemRepository itemRepository) {
         return args -> {
             Item item = new Item(
-                    "Kosula",
-                    13001L,
-                    25,
-                    "Bela Kosula",
+                    "Kostum",
+                    11001L,
+                    7950L,
+                    27,
                     LocalDate.now()
             );
-
             Item item1 = new Item(
                     "Sako",
                     13002L,
+                    9500L,
                     20,
-                    "Crno Sako",
                     LocalDate.now()
             );
             Item item2 = new Item(
-                    "Pantoloni",
-                    13003L,
+                    "Kostum",
+                    13005L,
+                    6150L,
                     45,
-                    "Crni Pantoloni",
+                    LocalDate.now()
+            );
+            Item item3 = new Item(
+                    "Kostum",
+                    13006L,
+                    5200L,
+                    50,
+                    LocalDate.now()
+            );
+            Item item4 = new Item(
+                    "Kostum",
+                    13008L,
+                    8500L,
+                    50,
+                    LocalDate.now()
+            );
+            Item item5 = new Item(
+                    "Kostum",
+                    13018L,
+                    7700L,
+                    50,
+                    LocalDate.now()
+            );
+            Item item6 = new Item(
+                    "Kostum",
+                    13019L,
+                    6850L,
+                    50,
+                    LocalDate.now()
+            );
+            Item item7 = new Item(
+                    "Kostum",
+                    13020L,
+                    5500L,
+                    50,
+                    LocalDate.now()
+            );
+            Item item8 = new Item(
+                    "Kostum",
+                    13030L,
+                    10500L,
+                    50,
+                    LocalDate.now()
+            );
+            Item item9 = new Item(
+                    "Kostum",
+                    13031L,
+                    9900L,
+                    50,
                     LocalDate.now()
             );
 
             itemRepository.saveAll(
-                    List.of(item,item1,item2)
+                    List.of(item,item1,item2,item3,item4,item5,item6,item7,
+                    item8,item9)
             );
         };
     }
