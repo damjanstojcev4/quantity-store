0f480db9de114ce41b5d3ff888cf5aed90506e4a
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-08-18 11:27:21 +0200

    FindBYSKU Service

--- quantity-store/src/main/java/com/damjan/quantitystore/service/ItemService.java
+++ quantity-store/src/main/java/com/damjan/quantitystore/service/ItemService.java
@@ -1,8 +1,6 @@
 package com.damjan.quantitystore.service;
 
-import com.damjan.quantitystore.domain.History;
 import com.damjan.quantitystore.domain.Item;
-import com.damjan.quantitystore.repository.HistoryRepository;
 import com.damjan.quantitystore.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,40 +10,35 @@ import java.util.List;
 @Service
 public class ItemService {
     private final ItemRepository itemRepository;
-    private final HistoryRepository historyRepository;
 
     @Autowired
-    public ItemService(ItemRepository itemRepository, HistoryRepository historyRepository) {
+    public ItemService(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
-        this.historyRepository = historyRepository;
-    }
-
-    public Item addItem(Item item) {
-        return itemRepository.save(item);
     }
 
     public List<Item> getAllItems() {
         return itemRepository.findAll();
     }
 
-//    public Item getItemById(Integer id) {
-//        return itemRepository.findById(id).orElse(null);
-//    }
-//
-//    public void updateItemQuantity(Integer itemId, int newQuantity) {
-//        Item item = itemRepository.findById(itemId).orElse(null);
-//        if (item != null) {
-//            int oldQuantity = item.getQuantity();
-//            item.setQuantity(newQuantity);
-//            itemRepository.updateQuantity(newQuantity);
-//
-//            // Update History's currentQuantity
+    public Item addItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public Item getItemBySku(Long sku) {
+        return itemRepository.findBySku(sku);
+    }
+
+    public void updateItemQuantity(Integer itemId, int newQuantity) {
+        Item item = itemRepository.findById(itemId).orElse(null);
+        if (item != null) {
+            int oldQuantity = item.getQuantity();
+            item.setQuantity(newQuantity);
+            itemRepository.updateQuantity(newQuantity);
+
+            // Update History's currentQuantity
 //            History history = new History();
 //            history.setCurrentQuantity(newQuantity);
 //            historyRepository.updateQuantity(newQuantity);
-//        }
-//    }
-    public List<Item> getItemBySku(Long sku) {
-        return itemRepository.findBySku(sku);
+        }
     }
 }
