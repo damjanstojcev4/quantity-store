c6604299464708e93e4318fcd959d244b6340abc
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-08-21 11:04:18 +0200

    Add Item controller

--- quantity-store/src/main/java/com/damjan/quantitystore/controller/ItemController.java
+++ quantity-store/src/main/java/com/damjan/quantitystore/controller/ItemController.java
@@ -5,6 +5,7 @@ import com.damjan.quantitystore.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -23,6 +24,11 @@ public class ItemController {
     @GetMapping("/{sku}")
     public Item getItemBySku(@PathVariable Long sku) {
         return itemService.getItemBySku(sku);
+    }
+
+    @PostMapping("/add")
+    public Item addArticle(@RequestBody Item item) {
+        return itemService.addItem(item);
     }
 
 }
