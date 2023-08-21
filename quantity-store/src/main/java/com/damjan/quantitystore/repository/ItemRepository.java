bc55decf41442dfb43d37e45e5266a146cd695ee
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-08-21 11:05:38 +0200

    Updated query

--- quantity-store/src/main/java/com/damjan/quantitystore/repository/ItemRepository.java
+++ quantity-store/src/main/java/com/damjan/quantitystore/repository/ItemRepository.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,7 +16,8 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
     List<Item>findAll();
     Item findBySku(Long sku);
     @Modifying
-    @Query("UPDATE Item i SET i.quantity = :newQuantity WHERE i.itemId = :itemId")
-    void updateQuantity(Integer itemId);
+    @Query("UPDATE Item i SET i.quantity = :quantity WHERE i.sku = :sku")
+    void updateQuantityBySku(@Param("sku") Long sku, @Param("quantity") int quantity);
+
 
 }
