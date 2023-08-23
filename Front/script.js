b048c6878d105b587168cd032aaac7a040516456
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-08-23 09:04:42 +0200

    Front- modified

--- Front/script.js
+++ Front/script.js
@@ -8,7 +8,6 @@ let init = {
     }
  };
 
-// Function to create and populate the table
 function createTable(data) {
     const tableBody = document.getElementById('table-body');
 
@@ -23,18 +22,40 @@ function createTable(data) {
     });
 }
 
-// Load data using fetch
 fetch(url)
     .then(response => response.json())
     .then(data => createTable(data))
     .catch(error => console.error('Error fetching data:', error));
 
+function loadOneSku(event) {
+        if (event.key == 'Enter') {
+            event.preventDefault();
+    
+            let skuInput = document.getElementById("sku");
+            let sku = skuInput.value;
+            
+            console.log(url + sku);
+    
+            fetch(url + sku)
+                .then(response => response.json())
+                .then(json => {
+                    prepareArticle(json);
+                    createTable(json);
+                })
+                .catch(error => {
+                    console.error("Error fetching SKU:", error);
+                });
+        }
+}
 
-const editButtons = document.querySelectorAll('.editButtonHome');
-editButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        // Handle edit button click here
-        // For example, you can open a modal for editing
-        // or navigate to a dedicated edit page.
-    });
-});
+function prepareArticle(json) {
+    document.getElementById("article").value = json.articleName;
+    document.getElementById("sku").value = json.sku;
+    document.getElementById("quantity").value = json.quantity;
+    
+    item = {
+       articleName: json.articleName,
+       sku: json.sku,
+       quantity: json.quantity
+    }
+ }
